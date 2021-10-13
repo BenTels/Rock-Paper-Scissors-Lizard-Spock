dbb3fd97897ba44d445087b08cf39ebdc3952826
Benjamin Ze'ev Tels <bzt@bentels.nl>
2021-10-13 20:24:37 +0200

    Added CLI args

--- rpsls_rust/src/main.rs
+++ rpsls_rust/src/main.rs
@@ -1,10 +1,30 @@
 mod game;
 mod interaction;
 
+use std::env;
 use interaction::input_src::*;
 
 fn main() {
-    let mut inp: InputSource = InputSource::STDIN;
+    let mut inp: InputSource = get_input_source();
+    play_the_game(&mut inp)
+}
+
+fn get_input_source() -> InputSource {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == (3 as usize) && has_file_args(&args) {
+        let file_path = &args[2];
+        return InputSource::from_file(file_path.trim());
+    }
+    InputSource::STDIN
+}
+
+fn has_file_args(args: &Vec<String>) -> bool {
+    (args[1] == "-f" || args[1] == "--file")
+        &&
+        !args[2].is_empty()
+}
+
+fn play_the_game(mut inp: &mut InputSource) {
     play(&mut inp);
     while inp.has_next_role() {
         play(&mut inp);
